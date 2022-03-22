947e35cc6027b7c8630062efadda84449004b235
Kate Montgomery <katemontgomery431@gmail.com>
2022-03-22 19:06:48 -0400

    commit tree 30333e98dd898aab2e6a95cd0a45e7d435334fa1

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string;
+  }
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    // todo
+  }
+  function whisper(string) {
+    return string;
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+function logShout(string) {
+      return string.toUpperCase
+  }
+function logShout(string) {
+    console.log(shout(string));
+}  
+function logWhisper(string) {
+    return string.toLowerCase
+}
+function logWhisper(string) {
+  console.log(whisper(string));
+}
+function sayHiToHeadphonedRoommate(string) {
+    if(string=="Let\'s have dinner together!"){return "I would love to!"}
+    if(string==shout(string)){return "YES INDEED!"}
+    if(string==whisper(string)){return "I can\'t hear you!"}
+}
+
